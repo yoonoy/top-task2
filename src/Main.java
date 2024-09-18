11740d89d5788ff94e73a295010ac5152638d140
aiganysh <aiganysh.abdukaarova@alatoo.edu.kg>
2024-09-19 00:55:57 +0600

    second

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,54 @@
 class Book {
-    String title;
-    String author;
-    String ISBN;
-    int price;
+
+    private String title;
+    private String author;
+    private String ISBN;
+    private int price;
+
+    public Book(){
+        this.title = "";
+        this.author = "";
+        this.ISBN = "";
+        this.price = 1;
+    }
+
+    public Book(String title, String author, String ISBN, int price){
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.price = price;
+    }
 
     void displayInfo() {
         System.out.println("Title: " + title);
         System.out.println("Author: " + author);
         System.out.println("ISBN: " + ISBN);
-        System.out.println("Price: " + price);
+        System.out.println("Price: $" + price);
     }
-}
 
-public class Main {
+
+    public void applyDiscount(double discount) {
+        price -= price * (discount / 100);
+    }
+
     public static void main(String[] args) {
+        Book book1 = new Book();
+        book1.displayInfo();
 
-        Book book = new Book();
-        book.title = "Harry Potter";
-        book.author = "JK Rowling";
-        book.ISBN = "6748392";
-        book.price = 2000;  // Correcting price to be an integer
+        System.out.println();
 
-        // Displaying the book information
-        book.displayInfo();
+        Book book2 = new Book("Harry Potter", "J.K. Rowling", "9780316769488", 20);
+        book2.displayInfo();
+
+        System.out.println();
+        book2.applyDiscount(15);
+        System.out.println("After applying a 15% discount:");
+        book2.displayInfo();
+
+        System.out.println();
+
+        Book book3 = new Book("Idiot", "Fyodor Dostoevsky", "7843689237402", 18);
+        book3.displayInfo();
     }
 }
+
